89844ff874e27606440161fd64969461d24b5ae9
Cory Borrow <cborrow03@gmail.com>
2025-08-06 21:58:48 -0500

    Cleaned up formatting

--- UserPrefs.php
+++ UserPrefs.php
@@ -1,8 +1,8 @@
 <?php
 
-if(!function_exists('str_starts_with')) {
+if (! function_exists('str_starts_with')) {
 	function str_starts_with($haystack, $needle) {
-		if(substr($haystack, 0, strlen($needle)) == $needle) {
+		if (substr($haystack, 0, strlen($needle)) == $needle) {
 			return true;
 		}
 		return false;
@@ -17,46 +17,46 @@ class UserPrefs {
 	protected $_blacklist;
 	protected $_customScores;
 
-	public $scoreThreshold = 5.0;
+	public $scoreThreshold    = 5.0;
 	public $subjectRewriteStr = "";
 
-	public function __construct($path) { 
+	public function __construct($path) {
 		$this->_userConfigPath = $path;
 	}
 
 	public function exists() {
-		if($this->_userConfigPath != null && file_exists($this->_userConfigPath)) {
+		if ($this->_userConfigPath != null && file_exists($this->_userConfigPath)) {
 			return true;
 		}
 		return false;
 	}
 
 	public function lastModTime() {
-		if($this->exists()) {
+		if ($this->exists()) {
 			return filemtime($this->_userConfigPath);
 		}
 		return 0;
 	}
 
 	public function lastAccessTime() {
-		if($this->exists()) {
+		if ($this->exists()) {
 			return fileatime($this->_userConfigPath);
 		}
 		return 0;
 	}
- 
+
 	public function write() {
-		if(!file_exists($this->_userConfigPath)) {
+		if (! file_exists($this->_userConfigPath)) {
 			touch($this->_userConfigPath);
 		}
 
 		$fp = fopen($this->_userConfigPath, "w+");
-		
-		if($fp != null && flock($fp, LOCK_EX) !== false) {
+
+		if ($fp != null && flock($fp, LOCK_EX) !== false) {
 			fwrite($fp, "## Basic user settings\n");
 			fwrite($fp, "required_score {$this->scoreThreshold}\n");
-			
-			if($this->subjectRewriteStr != null && is_string($this->subjectRewriteStr) 
+
+			if ($this->subjectRewriteStr != null && is_string($this->subjectRewriteStr)
 				&& strlen($this->subjectRewriteStr) > 0) {
 				fwrite($fp, "rewrite_header Subject {$this->subjectRewriteStr}");
 			}
@@ -64,30 +64,29 @@ class UserPrefs {
 			fwrite($fp, "\n\n");
 			fwrite($fp, "## Custom scores\n");
 
-			foreach($this->_customScores as $scoreName => $scoreValue) {
+			foreach ($this->_customScores as $scoreName => $scoreValue) {
 				fwrite($fp, "score {$scoreName} {$scoreValue}");
 			}
 
 			fwrite($fp, "\n\n");
 			fwrite($fp, "## User whitelist entries\n");
 
-			foreach($this->_whitelist as $wl) {
+			foreach ($this->_whitelist as $wl) {
 				fwrite($fp, "whitelist_from {$wl}\n");
 			}
 
 			fwrite($fp, "\n\n");
 			fwrite($fp, "## User blacklist entries\n");
 
-			foreach($this->_blacklist as $bl) {
+			foreach ($this->_blacklist as $bl) {
 				fwrite($fp, "blacklist_from {$bl}\n");
 			}
-		}
-		else {
+		} else {
 			Log::append("Failed to create lock for file {$this->_userConfigPath}", "Error");
 			//Return an error
 		}
 
-		if($fp != null && fstat($fp) !== false) {
+		if ($fp != null && fstat($fp) !== false) {
 			fclose($fp);
 		}
 	}
@@ -95,38 +94,32 @@ class UserPrefs {
 	public function read() {
 		$lines = file($this->_userConfigPath);
 
-		foreach($lines as $linenum => $line) {
+		foreach ($lines as $linenum => $line) {
 			#$line = $this->convertWhitespace($line);
 
-			if(str_starts_with($line, "#") || strlen($line) < 5) {
+			if (str_starts_with($line, "#") || strlen($line) < 5) {
 				continue;
-			}
-			else {
+			} else {
 				$line_parts = explode(" ", $line);
 
-				if(str_starts_with($line, "blacklist_from")) {
+				if (str_starts_with($line, "blacklist_from")) {
 					$this->_blacklist[] = $line_parts[1];
-				}
-				else if(str_starts_with($line, "whitelist_from")) {
+				} else if (str_starts_with($line, "whitelist_from")) {
 					$this->_whitelist[] = $line_parts[1];
-				}
-				else if(str_starts_with($line, "rewrite_header")) {
-					if(strtolower($line_parts[1]) != "subject") {
+				} else if (str_starts_with($line, "rewrite_header")) {
+					if (strtolower($line_parts[1]) != "subject") {
 						continue;
-					}
-					else {
-						if(count($line_parts) > 3) {
-							for($i = 3; $i < count($line_parts); $i++) {
-								$line_parts[2] += " " + $line_parts[$i];
+					} else {
+						if (count($line_parts) > 3) {
+							for ($i = 3; $i < count($line_parts); $i++) {
+								$line_parts[2] += " "+$line_parts[$i];
 							}
 						}
 						$this->subjectRewriteStr = $line_parts[2];
 					}
-				}
-				else if(str_starts_with($line, "required_score")) {
+				} else if (str_starts_with($line, "required_score")) {
 					$this->scoreThreshold = $line_parts[1];
-				}
-				else if(str_starts_with($line, "score")) {
+				} else if (str_starts_with($line, "score")) {
 					$this->addCustomScore($line_parts[1], $line_parts[2]);
 				}
 			}
@@ -138,15 +131,15 @@ class UserPrefs {
 	}
 
 	public function addCustomScore($name, $value) {
-		if(array_key_exists($this->_customScores, $name)) {
+		if (array_key_exists($this->_customScores, $name)) {
 			Log::append("Custom score {$name} already exists. Replacing {$this->_customScores[$name]} with {$value}");
 		}
-		
-		if(!is_numeric($value)) {
+
+		if (! is_numeric($value)) {
 			Log::append("Invalid value provided, most be a valid floating point number");
 			return false;
 		}
-		
+
 		$this->_customScores[$name] = $value;
 		return true;
 	}
@@ -156,17 +149,15 @@ class UserPrefs {
 	}
 
 	public function addWhitelist($value) {
-		if($this->isNullOrEmpty($value)) {
+		if ($this->isNullOrEmpty($value)) {
 			return false;
 		}
 
-		if($this->contains($value, $this->_whitelist)) {
+		if ($this->contains($value, $this->_whitelist)) {
 			Log::append("A whitelist entry for {$value} already exists");
-		}
-		else if($this->contains($value, $this->_blacklist)) {
+		} else if ($this->contains($value, $this->_blacklist)) {
 			Log::apend("A blacklist entry for {$value} already exists, ignoring.", "Warning");
-		}
-		else {
+		} else {
 			$this->_whitelist[] = $value;
 			return true;
 		}
@@ -174,8 +165,8 @@ class UserPrefs {
 	}
 
 	public function removeWhitelist($value) {
-		for($i = 0; $i < count($this->_whitelist); $i++) {
-			if($this->_whitelist == $value) {
+		for ($i = 0; $i < count($this->_whitelist); $i++) {
+			if ($this->_whitelist == $value) {
 				unset($this->_whitelist[$i]);
 			}
 		}
@@ -187,17 +178,15 @@ class UserPrefs {
 	}
 
 	public function addBlacklist($value) {
-		if($this->isNullOrEmpty($value)) {
+		if ($this->isNullOrEmpty($value)) {
 			return false;
 		}
 
-		if($this->contains($value, $this->_blacklist)) {
+		if ($this->contains($value, $this->_blacklist)) {
 			Log::append("A blacklist entry for {$value} already exists");
-		}
-		else if($this->contains($value, $this->_whitelist)) {
+		} else if ($this->contains($value, $this->_whitelist)) {
 			Log::append("A whitelist entry for {$value} already exists, ignoring.", "Warning");
-		}
-		else {
+		} else {
 			$this->_blacklist[] = $value;
 			return true;
 		}
@@ -205,8 +194,8 @@ class UserPrefs {
 	}
 
 	public function removeBlacklist($value) {
-		for($i = 0; $i < count($this->_blacklist); $i++) {
-			if($this->_blacklist[$i] == $value) {
+		for ($i = 0; $i < count($this->_blacklist); $i++) {
+			if ($this->_blacklist[$i] == $value) {
 				unset($this->_blacklist[$i]);
 			}
 		}
@@ -217,17 +206,17 @@ class UserPrefs {
 		$str = preg_replace("/\s{2,}/gm", " ", $str);
 		return $str;
 	}
-	
+
 	protected function isNullOrEmpty($str) {
-		if($str == null || strlen($str) == 0) {
+		if ($str == null || strlen($str) == 0) {
 			return true;
 		}
 		return false;
 	}
 
 	protected function contains($val, $array) {
-		foreach($array as $item) {
-			if($val == $item) {
+		foreach ($array as $item) {
+			if ($val == $item) {
 				return true;
 			}
 		}
