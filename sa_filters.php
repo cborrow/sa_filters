51f7550d8ee3e1f000c22788339a6c4b129ca575
Cory Borrow <cborrow03@gmail.com>
2025-08-09 14:42:40 -0500

    Finished cleanup of UserPrefs. Fixed include path for Amavis.php

--- sa_filters.php
+++ sa_filters.php
@@ -2,8 +2,8 @@
 
 $config = [];
 
-if (file_exists('/usr/local/cwpsrv/var/services/users/modules/sa_filters/sa_filters/config.php')) {
-	include '/usr/local/cwpsrv/var/services/users/modules/sa_filters/sa_filters/config.php';
+if (file_exists('/usr/local/cwpsrv/var/services/user_files/modules/sa_filters/config.php')) {
+	include '/usr/local/cwpsrv/var/services/user_files/modules/sa_filters/config.php';
 } else {
 	$config['amavis']['host'] = 'localhost';
 	$config['amavis']['name'] = 'amavis';
@@ -11,7 +11,7 @@ if (file_exists('/usr/local/cwpsrv/var/services/users/modules/sa_filters/sa_filt
 	$config['amavis']['pass'] = 'secret';
 }
 
-include "/usr/local/cwpsrv/var/services/users/modules/sa_filters/Amavis.php";
+include "/usr/local/cwpsrv/var/services/user_files/modules/sa_filters/Amavis.php";
 
 $amavis = new Amavis();
 $amavis->loadConfig($config['amavis']);
@@ -19,32 +19,15 @@ $amavis->loadConfig($config['amavis']);
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$data = json_decode(file_get_contents("php://input"), true);
 
-	// if(isset($data['score_threshold'])) {
-	// 	$prefs->scoreThreshold = floatval($data['score_threshold']);
-	// }
-	// if(isset($data['prepend_subject'])) {
-	// 	$prefs->subjectPrependStr = $data['prepend_subject'];
-	// }
-
 	foreach ($data['whitelist'] as $wl) {
-		#$prefs->addWhitelist($wl);
 		$amavis->addToWhitelist($wl);
 	}
 
 	foreach ($data['blacklist'] as $bl) {
-		#$prefs->addBlacklist($bl);
 		$amavis->addToBlacklist($wl);
 	}
-
-	#$prefs->write();
 }
 
 $mod['user'] = $_SESSION['username'];
-#$mod['score'] = $prefs->scoreThreshold;
-#$mod['prepend'] = $prefs->subjectPrependStr;
-#$mod['whitelist'] = $prefs->getWhitelist();
-#$mod['blacklist'] = $prefs->getBlacklist();
-#$mod['last_mod_time'] = date ("M d, Y H:i:s", $prefs->lastModTime());
-
 $mod['whitelist'] = $amavis->getWhitelist();
 $mod['blacklist'] = $amavis->getBlacklist();
