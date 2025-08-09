e79205e585571e6015484343a60e5112e655b577
Cory Borrow <cborrow03@gmail.com>
2025-08-09 13:43:54 -0500

    Replaced UserPrefs with Amavis and updated saving and loading of lists

--- sa_filters.php
+++ sa_filters.php
@@ -1,39 +1,57 @@
 <?php
-include __DIR__ . "/sa_filters/UserPrefs.php";
-include __DIR__ . "/sa_filters/Log.php";
+#include __DIR__ . "/sa_filters/UserPrefs.php";
 
-$prefs = new UserPrefs("/home/{$_SESSION['username']}/.spamassassin/user_prefs");
+$config = [];
 
-if($_SERVER['REQUEST_METHOD'] == "POST") {
+if (file_exists(__DIR__ . '/sa_filters/config.php')) {
+	include __DIR__ . '/sa_filters/config.php';
+} else {
+	$config['amavis']['host'] = 'localhost';
+	$config['amavis']['name'] = 'amavis';
+	$config['amavis']['user'] = 'amavis';
+	$config['amavis']['pass'] = 'secret';
+}
+
+include __DIR__ . "/sa_filters/Amavis.php";
+
+#$prefs = new UserPrefs("/home/{$_SESSION['username']}/.spamassassin/user_prefs.cf");
+
+$amavis = new Amavis();
+$amavis->loadConfig($config['amavis']);
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$data = json_decode(file_get_contents("php://input"), true);
 
-	if(isset($data['score_threshold'])) {
-		$prefs->scoreThreshold = floatval($data['score_threshold']);
-	}
-	if(isset($data['prepend_subject'])) { 
-		$prefs->subjectPrependStr = $data['prepend_subject'];
+	// if(isset($data['score_threshold'])) {
+	// 	$prefs->scoreThreshold = floatval($data['score_threshold']);
+	// }
+	// if(isset($data['prepend_subject'])) {
+	// 	$prefs->subjectPrependStr = $data['prepend_subject'];
+	// }
+
+	foreach ($data['whitelist'] as $wl) {
+		#$prefs->addWhitelist($wl);
+		$amavis->addToWhitelist($wl);
 	}
 
-	foreach($data['whitelist'] as $wl) {
-		$prefs->addWhitelist($wl);
+	foreach ($data['blacklist'] as $bl) {
+		#$prefs->addBlacklist($bl);
+		$amavis->addToBlacklist($wl);
 	}
 
-	foreach($data['blacklist'] as $bl) {
-		$prefs->addBlacklist($bl);
-	}
-
-	$prefs->write();
+	#$prefs->write();
 }
 
-if($prefs->exists()) {
-	$prefs->read();
-	
+if ($prefs->exists()) {
+	#$prefs->read();
+
 	$mod['user'] = $_SESSION['username'];
-	$mod['score'] = $prefs->scoreThreshold;
-	$mod['prepend'] = $prefs->subjectPrependStr;
-	$mod['whitelist'] = $prefs->getWhitelist();
-	$mod['blacklist'] = $prefs->getBlacklist();
-	$mod['last_mod_time'] = date ("M d, Y H:i:s", $prefs->lastModTime());
-}
+	#$mod['score'] = $prefs->scoreThreshold;
+	#$mod['prepend'] = $prefs->subjectPrependStr;
+	#$mod['whitelist'] = $prefs->getWhitelist();
+	#$mod['blacklist'] = $prefs->getBlacklist();
+	#$mod['last_mod_time'] = date ("M d, Y H:i:s", $prefs->lastModTime());
 
-Log::flushPendingEntries();
+	$mod['whitelist'] = $amavis->getWhitelist();
+	$mod['blacklist'] = $amavis->getBlacklist();
+}
